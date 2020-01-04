5f6b88e9703a267e9a78481a6bf498cf00b858ab
Jon Botros <joneb@Jons-MacBook-Pro-2.local>
2020-01-04 17:33:06 -0500

    fixed caurosel

--- src/components/home/slideshow.js
+++ src/components/home/slideshow.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel'
+const styled = {
+        "background-image": require("../images/one.jpg"),
+        "background-position": "center",
+        "background-repeat": "no-repeat",
+        "background-size": "cover",
+        "height": "91vh",
+}
 
 export default function Slideshow() {
     const [index, setIndex] = useState(0);
@@ -13,36 +20,32 @@ export default function Slideshow() {
     return (
 
 
-        <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
-        <Carousel.Item>
+        <Carousel activeIndex={index} direction={direction} onSelect={handleSelect} class = "jumbotron-fluid ">
+        <Carousel.Item  style = {styled}>
             <img
-            src={require("../images/one.jpg")}
-            alt="First slide"
-            className = "d-block w-100"
+                src={require("../images/one.jpg")}
+                alt="First slide"
             />
             <Carousel.Caption>
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-            <img
+        <Carousel.Item  style = {styled}>
+             <img
             src={require("../images/two.jpg")}
             alt="Second slide"
-            className = "d-block w-100"
     
-            />
-    
+            /> 
             <Carousel.Caption>
             <h3>Second slide label</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
-            <img
+        <Carousel.Item  style = {styled}>
+           <img
             src={require("../images/three.jpg")}
             alt="Third slide"
-            className = "d-block w-100"
     
             />
     
